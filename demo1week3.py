75c0ad22432f565cd6c958817c0393d2faeebd90
Owen Lafont <owenlafont@gmail.com>
2020-09-20 10:51:26 -0400

    Initial commit

---
+++ demo1week3.py
@@ -0,0 +1,18 @@
+
+
+def main1():
+    print("This is in a function")
+    print("This is the second line")
+
+    age = 5
+    dog_years = age * 7
+
+    print(dog_years)
+
+    fake_age = "29"
+    print(fake_age)
+    updated_fake_age = fake_age*5
+    print(updated_fake_age)
+    print(10/3)
+
+main1()
